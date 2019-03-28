3b20e2ee20de4758acdb2a022709cef177c1c698
unknown <kevin.cheung.kai@gmail.com>
2019-03-28 17:33:08 -0400

    Added sound, button click, and animation

--- game.js
+++ game.js
@@ -1,11 +1,35 @@
-buttonColours = ['red', 'blue', 'green', 'yellow']
-gamePattern = []
+const buttonColours = ['red', 'blue', 'green', 'yellow']
+const gamePattern = []
+const userClickedPattern = []
+
+
+$('.btn').click(function(){
+  const userChosenColour = $(this).attr('id')
+  userClickedPattern.push(userChosenColour)
+
+  playSound(userChosenColour)
+})
 
 function nextSequence() {
   const randomNumber = Math.floor(Math.random()*4)
   const randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour)
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour)
 }
 
-nextSequence();
+function playSound(name) {
+  const colorAudio = new Audio('sounds/' + name + '.mp3');
+  colorAudio.play();
+}
+
+function animatePress(currentColour) {
+
+}
+
+
+
+console.log(userClickedPattern);
 console.log(gamePattern);
